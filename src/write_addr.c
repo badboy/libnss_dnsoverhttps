03ce14a7177408ba832347ffc4e9d19fcac24626
Jan-Erik Rediger <janerik@fnordig.de>
2018-02-14 20:06:16 +0100

    Set success status on successful run

--- src/write_addr.c
+++ src/write_addr.c
@@ -148,7 +148,7 @@ enum nss_status write_addresses3(
 	if (canonp)
 		*canonp = r_name;
 
-	return NSS_STATUS_NOTFOUND;
+	return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_dnsoverhttps_gethostbyname3_r(
